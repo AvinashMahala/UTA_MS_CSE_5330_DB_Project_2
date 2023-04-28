06178c450a3e96986cc7f37478d4f8967911177f
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-04-28 04:41:18 -0500

    getting connected to oracle db now!

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,59 +1,122 @@
-// This line imports configuration values from the .env file 
 import "dotenv/config";
-
-// These lines import required modules and files 
 import express, { Request, Response, NextFunction } from "express";
 import notesRoutes from "./routes/notes";
 import userRoutes from "./routes/users";
 import morgan from "morgan";
-import createHttpError, { isHttpError } from "http-errors";
+import createHttpError, { HttpError, isHttpError } from "http-errors";
 import session from "express-session";
+import oracledb from "oracledb";
 import env from "./util/validateenv";
-import MongoStore  from "connect-mongo";
 import { requiresAuth } from "./middleware/auth";
+
+interface SessionData extends session.SessionData {
+  [key: string]: any;
+}
 
 const app = express();
 
-// This middleware logs HTTP requests to the console with a specified format
 app.use(morgan("dev"));
 
-// This middleware parses incoming request data in JSON format and adds it to req.body
 app.use(express.json());
 
-// This middleware sets up express-session with options for session handling
-app.use(session({
-    secret: env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie:{
-        maxAge: 60 * 60 * 1000, 
-    },
-    rolling: true,
-    store: MongoStore.create({
-        mongoUrl: env.MONGO_CONNECTION_STRING
-    })
-}));
+// Connect to Oracle database using a connection pool
+oracledb
+  .createPool({
+    user: env.ORACLE_DB_USER,
+    password: env.ORACLE_DB_PASSWORD,
+    connectString: env.ORACLE_DB_CONNECTION_STRING,
+    poolMin: 10,
+    poolMax: 10,
+  })
+  .then((pool) => {
+    console.log("Oracle Connection Pool Created!");
 
-// These routes handle API endpoints for users and notes
-app.use("/api/users", userRoutes);
-app.use("/api/notes", requiresAuth, notesRoutes);
+    app.use(
+      session({
+        secret: env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          maxAge: 60 * 60 * 1000,
+        },
+        rolling: true,
+        store: new (class OracleDBStore extends session.Store {
+          pool: oracledb.Pool;
 
-// This middleware is used as a catch-all endpoint that triggers when no other route is matched
-app.use((req, res, next)=>{
-    next(createHttpError(404,"Endpoint not Found!!"));
-});
+          constructor() {
+            super();
+            this.pool = oracledb.getPool();
+          }
 
-// This middleware is used as an error handler that catches any uncaught errors and sends an appropriate response back to the client
-app.use((error:unknown, req:Request, res:Response, next:NextFunction)=>{
-    //console.log(error);
-    let errorMessage="An Unknown Error Occurred!";
-    let statusCode=500;
-    if(isHttpError(error)){
-        statusCode=error.status;
-        errorMessage=error.message;
-    }
-    res.status(statusCode).json({error: errorMessage});
-});
+          async set(sid: string, sessionData: SessionData) {
+            const conn = await this.pool.getConnection();
+            try {
+              const result = await conn.execute(
+                "MERGE INTO sessions USING DUAL ON (sid = :1) WHEN MATCHED THEN UPDATE SET data = :2 WHEN NOT MATCHED THEN INSERT (sid, data) VALUES (:1, :2)",
+                [sid, JSON.stringify(sessionData)]
+              );
+              if (result.rowsAffected === 0) {
+                throw new Error("Unable to set session data");
+              }
+            } finally {
+              conn.release();
+            }
+          }
 
-// This exports the express app instance for use in other modules
+          async get(sid: string) {
+            const conn = await this.pool.getConnection();
+            try {
+              const result = await conn.execute(
+                "SELECT data FROM sessions WHERE sid = :1",
+                [sid]
+              );
+              if ((result as any).rows && (result as any).rows.length > 0) {
+                return JSON.parse((result as any).rows[0][0]) as SessionData;
+              } else {
+                return null;
+              }
+            } finally {
+              conn.release();
+            }
+          }
+
+          async destroy(sid: string) {
+            const conn = await this.pool.getConnection();
+            try {
+              const result = await conn.execute(
+                "DELETE FROM sessions WHERE sid = :1",
+                [sid]
+              );
+              if (result.rowsAffected === 0) {
+                throw new Error("Unable to destroy session data");
+              }
+            } finally {
+              conn.release();
+            }
+          }
+        })(),
+      })
+    );
+
+    app.use("/api/users", userRoutes);
+    app.use("/api/notes", requiresAuth, notesRoutes);
+
+    app.use((req, res, next) => {
+      next(createHttpError(404, "Endpoint not Found!!"));
+    });
+
+    app.use(
+      (error: unknown, req: Request, res: Response, next: NextFunction) => {
+        let errorMessage = "An Unknown Error Occurred!";
+        let statusCode = 500;
+        if (isHttpError(error)) {
+          statusCode = error.status;
+          errorMessage = error.message;
+        }
+        res.status(statusCode).json({ error: errorMessage });
+      }
+    );
+  })
+  .catch(console.error);
+
 export default app;
