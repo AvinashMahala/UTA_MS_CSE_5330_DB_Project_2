1e5c72eca3ab92a034eb343c1664a28b9d20f6e1
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-04-29 23:52:05 -0500

    worked with inline sample data

--- frontend/src/components/customers/RetrieveAllCustomersView.tsx
+++ frontend/src/components/customers/RetrieveAllCustomersView.tsx
@@ -1,99 +1,284 @@
-import { useEffect, useState } from "react";
-import { Button, Col, Row, Spinner } from "react-bootstrap";
-import { FaPlus } from "react-icons/fa";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
 import { Customer as CustomerModel } from "../../models/Customer";
 import * as CustomerApi from "../../network/customer_api";
-//import styles from "../styles/NotesPage.module.css";
-import styleUtils from "../styles/utils.module.css";
-import MaterialReactTable, { MRT_ColumnDef } from "material-react-table";
-import React, { useMemo } from "react";
 import { darken } from "@mui/material";
 
-const RetrieveAllCustomersView = () => {
-  const [customers, setCustomers] = useState<CustomerModel[]>([]);
-  const [customersLoading, setCustomersLoading] = useState(true);
-  const [showCustomersLoadingError, setShowCustomersLoadingError] =
-    useState(false);
 
-  useEffect(() => {
-    async function loadCustomers() {
-      try {
-        setShowCustomersLoadingError(false);
-        setCustomersLoading(true);
-        const customers = await CustomerApi.fetchCustomers();
-        setCustomers(customers);
-      } catch (error) {
-        console.error(error);
-        setShowCustomersLoadingError(true);
-      } finally {
-        setCustomersLoading(false);
-      }
-    }
-    loadCustomers().then(()=>{
-        console.log("loadCustomers().then");
-        console.log(customers);
-    });
-  }, []);
 
-  //column definitions - strongly typed if you are using TypeScript (optional, but recommended)
-  const columns = useMemo<MRT_ColumnDef<CustomerModel>[]>(
-    () => [
-      {
-        accessorKey: "idNo", //simple recommended way to define a column
-        header: "ID No",
-        muiTableHeadCellProps: {
-             sx: { color: "green" } ,
-             align: "left",
-            }, //custom props
-      },
-      {
-        accessorFn: (originalRow) => originalRow.name, //alternate way
-        id: "Name", //id required if you use accessorFn instead of accessorKey
-        header: "Name",
-        Header: <i style={{ color: "red" }}>Name</i>, //optional custom markup
-        Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>, //optional custom cell renderer
-        muiTableHeadCellProps: {
-            sx: { color: "green" } ,
-            align: "left",
-           }, //custom props
-      },
-      {
-        accessorFn: (originalRow) => originalRow.phone, //alternate way
-        id: "Phone Number", //id required if you use accessorFn instead of accessorKey
-        header: "Phone",
-        Header: <i style={{ color: "red" }}>Phone</i>, //optional custom markup
-        muiTableHeadCellProps: {
-            sx: { color: "green" } ,
-            align: "left",
-           }, //custom props
-      },
-    ],
-    []
-  );
 
-  return (
-    
-    <>
-    <MaterialReactTable
-      columns={columns}
-      data={customers}
-      muiTablePaperProps={{
-        elevation: 0,
-        sx: {
-          borderRadius: '0',
-          border: '1px dashed #e0e0e0',
-        },
-      }}
-      muiTableBodyProps={{
-        sx: (theme) => ({
-          '& tr:nth-of-type(odd)': {
-            backgroundColor: darken(theme.palette.background.default, 0.1),
-          },
-        }),
-      }}
-    />
-    </>
-  );
+
+import MaterialReactTable, {
+  type MaterialReactTableProps,
+  type MRT_Cell,
+  type MRT_ColumnDef,
+  type MRT_Row,
+} from 'material-react-table';
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  MenuItem,
+  Stack,
+  TextField,
+  Tooltip,
+} from '@mui/material';
+import { Delete, Edit } from '@mui/icons-material';
+
+export type Person = {
+    idNo: number,
+    name: string,
+    phone: string,
 };
 
-export default RetrieveAllCustomersView;
+
+const RetrieveAllCustomersView = () => {
+
+    const data=[
+        {
+            idNo: 201,
+            name:"J.Smith",
+            phone:"817-272-3000",
+        },
+        {
+            idNo: 202,
+            name:"J.Doe",
+            phone:"817-272-3001",
+        },
+        {
+            idNo: 203,
+            name:"J.Doe",
+            phone:"817-272-3002",
+        },
+    ]
+
+    const states = useMemo<string[]>(() => ['CA', 'NY', 'TX', 'FL'], []);
+
+
+
+
+    const [createModalOpen, setCreateModalOpen] = useState(false);
+    const [tableData, setTableData] = useState<Person[]>(() => data);
+    const [validationErrors, setValidationErrors] = useState<{
+      [cellId: string]: string;
+    }>({});
+  
+    const handleCreateNewRow = (values: Person) => {
+      tableData.push(values);
+      setTableData([...tableData]);
+    };
+  
+    const handleSaveRowEdits: MaterialReactTableProps<Person>['onEditingRowSave'] =
+      async ({ exitEditingMode, row, values }) => {
+        if (!Object.keys(validationErrors).length) {
+          tableData[row.index] = values;
+          //send/receive api updates here, then refetch or update local table data for re-render
+          setTableData([...tableData]);
+          exitEditingMode(); //required to exit editing mode and close modal
+        }
+      };
+  
+    const handleCancelRowEdits = () => {
+      setValidationErrors({});
+    };
+  
+    const handleDeleteRow = useCallback(
+      (row: MRT_Row<Person>) => {
+        if (
+          !window.confirm(`Are you sure you want to delete ${row.getValue('firstName')}`)
+        ) {
+          return;
+        }
+        //send api delete request here, then refetch or update local table data for re-render
+        tableData.splice(row.index, 1);
+        setTableData([...tableData]);
+      },
+      [tableData],
+    );
+  
+    const getCommonEditTextFieldProps = useCallback(
+      (
+        cell: MRT_Cell<Person>,
+      ): MRT_ColumnDef<Person>['muiTableBodyCellEditTextFieldProps'] => {
+        return {
+          error: !!validationErrors[cell.id],
+          helperText: validationErrors[cell.id],
+          onChange: (e) => {
+            const value = e.target.value;
+            if (!value) {
+              setValidationErrors((prev) => ({
+                ...prev,
+                [cell.id]: 'Required',
+              }));
+            } else {
+              setValidationErrors((prev) => {
+                const next = { ...prev };
+                delete next[cell.id];
+                return next;
+              });
+            }
+            //cell.setEditingCellValue(value);
+          },
+        };
+      },
+      [validationErrors],
+    );
+  
+    const columns = useMemo<MRT_ColumnDef<Person>[]>(
+      () => [
+        {
+          accessorKey: 'idNo',
+          header: 'ID',
+          enableColumnOrdering: false,
+          enableEditing: false, //disable editing on this column
+          enableSorting: false,
+          size: 80,
+        },
+        {
+          accessorKey: 'name',
+          header: 'Name',
+          size: 140,
+          muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+            ...getCommonEditTextFieldProps(cell),
+          }),
+        },
+        {
+          accessorKey: 'phone',
+          header: 'Phone Number',
+          size: 140,
+          muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+            ...getCommonEditTextFieldProps(cell),
+          }),
+        },
+      ],
+      [getCommonEditTextFieldProps],
+    );
+  
+    return (
+      <>
+        <MaterialReactTable
+          displayColumnDefOptions={{
+            'mrt-row-actions': {
+              muiTableHeadCellProps: {
+                align: 'center',
+              },
+              size: 120,
+            },
+          }}
+          columns={columns}
+          data={tableData}
+          editingMode="modal" //default
+          enableColumnOrdering
+          enableEditing
+          onEditingRowSave={handleSaveRowEdits}
+          onEditingRowCancel={handleCancelRowEdits}
+          renderRowActions={({ row, table }) => (
+            <Box sx={{ display: 'flex', gap: '1rem' }}>
+              <Tooltip arrow placement="left" title="Edit">
+                <IconButton onClick={() => table.setEditingRow(row)}>
+                  <Edit />
+                </IconButton>
+              </Tooltip>
+              <Tooltip arrow placement="right" title="Delete">
+                <IconButton color="error" onClick={() => handleDeleteRow(row)}>
+                  <Delete />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          )}
+          renderTopToolbarCustomActions={() => (
+            <Button
+              color="secondary"
+              onClick={() => setCreateModalOpen(true)}
+              variant="contained"
+            >
+              Create New Account
+            </Button>
+          )}
+        />
+        <CreateNewAccountModal
+          columns={columns}
+          open={createModalOpen}
+          onClose={() => setCreateModalOpen(false)}
+          onSubmit={handleCreateNewRow}
+        />
+      </>
+    );
+  };
+  
+  interface CreateModalProps {
+    columns: MRT_ColumnDef<Person>[];
+    onClose: () => void;
+    onSubmit: (values: Person) => void;
+    open: boolean;
+  }
+  
+  //example of creating a mui dialog modal for creating new rows
+  export const CreateNewAccountModal = ({
+    open,
+    columns,
+    onClose,
+    onSubmit,
+  }: CreateModalProps) => {
+    const [values, setValues] = useState<any>(() =>
+      columns.reduce((acc, column) => {
+        acc[column.accessorKey ?? ''] = '';
+        return acc;
+      }, {} as any),
+    );
+  
+    const handleSubmit = () => {
+      //put your validation logic here
+      onSubmit(values);
+      onClose();
+    };
+  
+    return (
+      <Dialog open={open}>
+        <DialogTitle textAlign="center">Create New Account</DialogTitle>
+        <DialogContent>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <Stack
+              sx={{
+                width: '100%',
+                minWidth: { xs: '300px', sm: '360px', md: '400px' },
+                gap: '1.5rem',
+              }}
+            >
+              {columns.map((column) => (
+                <TextField
+                  key={column.accessorKey}
+                  label={column.header}
+                  name={column.accessorKey}
+                  onChange={(e) =>
+                    setValues({ ...values, [e.target.name]: e.target.value })
+                  }
+                />
+              ))}
+            </Stack>
+          </form>
+        </DialogContent>
+        <DialogActions sx={{ p: '1.25rem' }}>
+          <Button onClick={onClose}>Cancel</Button>
+          <Button color="secondary" onClick={handleSubmit} variant="contained">
+            Create New Account
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+  
+  const validateRequired = (value: string) => !!value.length;
+  const validateEmail = (email: string) =>
+    !!email.length &&
+    email
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
+  const validateAge = (age: number) => age >= 18 && age <= 50;
+  
+  export default RetrieveAllCustomersView;
