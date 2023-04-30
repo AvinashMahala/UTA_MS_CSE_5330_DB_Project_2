20bb36c5dab5cef01f2d85ce7e754b52d23d53c1
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-04-29 22:01:56 -0500

    Update RetrieveAllCustomersView.tsx

--- frontend/src/components/customers/RetrieveAllCustomersView.tsx
+++ frontend/src/components/customers/RetrieveAllCustomersView.tsx
@@ -7,6 +7,7 @@ import * as CustomerApi from "../../network/customer_api";
 import styleUtils from "../styles/utils.module.css";
 import MaterialReactTable, { MRT_ColumnDef } from "material-react-table";
 import React, { useMemo } from "react";
+import { darken } from "@mui/material";
 
 const RetrieveAllCustomersView = () => {
   const [customers, setCustomers] = useState<CustomerModel[]>([]);
@@ -39,20 +40,32 @@ const RetrieveAllCustomersView = () => {
     () => [
       {
         accessorKey: "idNo", //simple recommended way to define a column
-        header: "idNo",
-        muiTableHeadCellProps: { sx: { color: "green" } }, //custom props
+        header: "ID No",
+        muiTableHeadCellProps: {
+             sx: { color: "green" } ,
+             align: "left",
+            }, //custom props
       },
       {
         accessorFn: (originalRow) => originalRow.name, //alternate way
-        id: "name", //id required if you use accessorFn instead of accessorKey
+        id: "Name", //id required if you use accessorFn instead of accessorKey
         header: "Name",
         Header: <i style={{ color: "red" }}>Name</i>, //optional custom markup
+        Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>, //optional custom cell renderer
+        muiTableHeadCellProps: {
+            sx: { color: "green" } ,
+            align: "left",
+           }, //custom props
       },
       {
         accessorFn: (originalRow) => originalRow.phone, //alternate way
-        id: "phone", //id required if you use accessorFn instead of accessorKey
+        id: "Phone Number", //id required if you use accessorFn instead of accessorKey
         header: "Phone",
         Header: <i style={{ color: "red" }}>Phone</i>, //optional custom markup
+        muiTableHeadCellProps: {
+            sx: { color: "green" } ,
+            align: "left",
+           }, //custom props
       },
     ],
     []
@@ -61,13 +74,24 @@ const RetrieveAllCustomersView = () => {
   return (
     
     <>
-      <MaterialReactTable
-        columns={columns}
-        data={customers}
-        enableRowSelection //enable some features
-        enableColumnOrdering
-        enableGlobalFilter={false} //turn off a feature
-      />
+    <MaterialReactTable
+      columns={columns}
+      data={customers}
+      muiTablePaperProps={{
+        elevation: 0,
+        sx: {
+          borderRadius: '0',
+          border: '1px dashed #e0e0e0',
+        },
+      }}
+      muiTableBodyProps={{
+        sx: (theme) => ({
+          '& tr:nth-of-type(odd)': {
+            backgroundColor: darken(theme.palette.background.default, 0.1),
+          },
+        }),
+      }}
+    />
     </>
   );
 };
